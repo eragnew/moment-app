04e0635199d9ef5e90a947aeb033cfbdef16f927
Peter Strumolo <Peter Strumolo>
2015-10-22 19:46:45 -0700

    Initial new release service

---
+++ src/app/js/services/spotify_release_service.js
@@ -0,0 +1,27 @@
+module.exports = function(app) {
+  app.service('SpotifyAPI', ['MomentsAPI', '$http', '$location', '$cookies', function(MomentsAPI, $http, $location, $cookies) {
+
+    var token = $cookies.get('token');
+    if (!(token && token.length)) {
+      $cookies.remove('token');
+      $location.path('/login');
+    }
+
+    var urlRoot = 'https://api.spotify.com/v1';
+
+    var apiSpotify = {
+      me: function() {
+        var req = {
+          method: 'GET',
+          url: urlRoot + '/browse/new-releases',
+          headers: {
+            'Authorization': 'Bearer ' + window.sessionStorage.getItem('access_token')
+          }
+        };
+        return $http(req);
+      }
+    };
+
+    return apiSpotify;
+  }]);
+};
