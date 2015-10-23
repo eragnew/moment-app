427ed2620b683eccae53db215c7daf4bd3a44ee7
Peter Strumolo <Peter Strumolo>
2015-10-22 22:58:05 -0700

    Initial albums for login

--- src/app/js/services/spotify_search_service.js
+++ src/app/js/services/spotify_search_service.js
@@ -1,11 +1,16 @@
 module.exports = function(app) {
   app.service('SpotifySearchService', ['$http', function($http) {
-    var urlRoot = 'https://api.spotify.com/v1/search?q=track:';
+    var urlRoot = 'https://api.spotify.com/v1/search?q=';
 
     var searchSpotify = {
-      get: function(q) {
+      getTrack: function(q) {
         if (angular.isDefined(q)) {
-          return $http.get(urlRoot + q + '&type=track');
+          return $http.get(urlRoot + 'track:' + q + '&type=track');
+        }
+      },
+      getAlbums: function(q) {
+        if (angular.isDefined(q)) {
+          return $http.get(urlRoot + 'year:' + q + '&type=album');
         }
       }
     };
