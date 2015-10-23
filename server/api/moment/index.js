64fddc7f57096fec2c6325bfc5e5417edd86fd38
Emmett Agnew <eragnew@gmail.com>
2015-10-22 19:40:51 -0700

    Fixed API

--- server/api/moment/index.js
+++ server/api/moment/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.index);
 router.post('/new', auth.isAuthenticated(), controller.create);
-// router.get('/:id', auth.isAuthenticated(), controller.show);
+router.get('/:id', auth.isAuthenticated(), controller.show);
 router.put('/:id', auth.isAuthenticated(), controller.update);
 router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 router.get('/stats', auth.isAuthenticated(), controller.stats);
