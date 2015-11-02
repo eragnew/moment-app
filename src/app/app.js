b594a026fcc005894e0aa811da11374c9f971624
Peter Strumolo <Peter Strumolo>
2015-11-02 13:36:34 -0500

    Update dependencies & delete misc code in app.js

--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,6 @@ require('angular-route');
 require('angular-cookies');
 require('angular-sanitize');
 require('showdown');
-// require('ng-showdown');
 
 var momentApp = angular.module('momentApp', ['ngRoute', 'ngCookies', 'ngSanitize']);
 
@@ -11,42 +10,3 @@ require('./js/services/services')(momentApp);
 require('./js/moments/moments')(momentApp);
 require('./js/users/users')(momentApp);
 require('./js/router')(momentApp);
-
-
-// require('angular');
-// // require('ng-showdown');
-// require('angular-route');
-
-// (function appFile() {
-
-//   "use strict";
-
-//   var app = angular.module('momentApp', ['ngRoute']);
-
-//   app.config(["$routeProvider", function($routeProvider) {
-
-//       $routeProvider.when('/moments', {
-//         templateUrl: "views/moments.html",
-//         controller: "MomentsCtrl as vm"
-//       }).
-//       when('/moments/new', {
-//         templateUrl: "views/posts/blog-post-form.html",
-//         controller: "BlogFormCtrl as vm",
-//       }).
-//       when('/moments/:gist_id', {
-//         templateUrl: "views/posts/blog-post-detail.html",
-//         controller: "BlogCtrl as vm"
-//       }).
-//        when('/moments/:gist_id/edit', {
-//         templateUrl: "views/posts/blog-post-form.html",
-//         controller: "BlogFormCtrl as vm"
-//       }).
-//        when('/moments/:gist_id/destroy', {
-//         templateUrl: "views/posts/blog-post-detail.html",
-//         controller: "BlogFormCtrl as vm"
-//       }).
-//        otherwise({
-//         redirectTo: "/moments"
-//       });
-//     }]);
-// })();
