3d9e26ad1a510c228e49c7137604b90e49ea3698
Peter Strumolo <Peter Strumolo>
2015-11-03 11:35:18 -0500

    Remove unnecessary function in moment controller

--- src/app/js/moments/momentdetail.ctrl.js
+++ src/app/js/moments/momentdetail.ctrl.js
@@ -3,10 +3,6 @@ module.exports = function(app) {
 
       var vm = this;
 
-      var toHtml = function(text) {
-        ShowdownService.makeHtml(text);
-      };
-
       var token = $cookies.get('token');
         if (!(token && token.length))
         $location.path('/login');
